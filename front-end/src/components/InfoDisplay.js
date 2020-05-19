27a9d5750d1a5cd8a576eb024954a54c0f3eb123
Alex Delitzas <alex.delitzas@gmail.com>
2020-05-19 12:19:54 +0300

    Add Info section

--- front-end/src/components/InfoDisplay.js
+++ front-end/src/components/InfoDisplay.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react'
 import '../styles/InfoDisplay.css'
+import Item1 from '../assets/item1.png'
+import Item2 from '../assets/item2.png'
+import Item3 from '../assets/item3.png'
 
 class InfoDisplay extends Component {
-
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-
-//       }
-//     }
-
 
   render() {
 
     return (
-        <div className="info-section">
-            <div className="info-title">
-                <p>
-                    Providing<br />Website Aesthetics As a Service.
-                </p>
-            </div>
+      <div className="info-section">
+        <div className="info-title">
+          <p>
+            Providing<br />Website Aesthetics As a Service.
+          </p>
         </div>
+        <div className="info-items">
+          <div className="info-item">
+            <div className="info-item-text">
+              <div className="info-item-title">
+                <p>A tool from developers to developers</p>
+              </div>
+              <div className="info-item-description">
+                <p>
+                  Here goes text. Here goes text. Here goes text. Here goes text. Here goes text.
+                </p>
+              </div>
+            </div>
+            <div className="info-item-image">
+              <img src={Item1} alt="" />
+            </div>
+          </div>
+          <div className="info-item-reverse">
+            <div className="info-item-reverse-image">
+              <img src={Item2} alt="" />
+            </div>
+            <div className="info-item-reverse-text">
+              <div className="info-item-title">
+                <p>AI-powered</p>
+              </div>
+              <div className="info-item-description">
+                <p>
+                  Here goes text. Here goes text. Here goes text. Here goes text. Here goes text.
+                </p>
+              </div>
+            </div>  
+          </div>
+          <div className="info-item">
+            <div className="info-item-text">
+              <div className="info-item-title">
+                <p>Constantly improving</p>
+              </div>
+              <div className="info-item-description">
+                <p>
+                  Here goes text. Here goes text. Here goes text. Here goes text. Here goes text.
+                </p>
+              </div>
+            </div>
+            <div className="info-item-image">
+              <img src={Item3} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     )
     
   }
