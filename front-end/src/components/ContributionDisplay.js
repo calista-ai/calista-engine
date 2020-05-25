e18aa6eca14bf0d686c3803fa28191d0a9049e77
Alex Delitzas <alex.delitzas@gmail.com>
2020-05-25 17:23:15 +0300

    Add Contribution section

---
+++ front-end/src/components/ContributionDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../styles/ContributionDisplay.css'
+
+class ContributionDisplay extends Component {
+
+  render() {
+
+    return (
+      <div className="contribution-section">
+
+      </div>
+    )
+    
+  }
+}
+
+export default ContributionDisplay
