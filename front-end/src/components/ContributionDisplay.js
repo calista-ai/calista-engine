caa27ce5a0ed7f217f133620db336f3b331986f0
Alex Delitzas <alex.delitzas@gmail.com>
2020-05-26 14:39:57 +0300

    Add contribution section styling

--- front-end/src/components/ContributionDisplay.js
+++ front-end/src/components/ContributionDisplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../styles/ContributionDisplay.css'
+import Item4 from '../assets/item4.gif'
 
 class ContributionDisplay extends Component {
 
@@ -7,7 +8,28 @@ class ContributionDisplay extends Component {
 
     return (
       <div className="contribution-section">
-
+        
+        <div className="contribution-container">
+          <div className="contribution-section-image">
+            <img src={Item4} alt="" />
+          </div>
+          <div className="contribution-container-inner">
+            <div className="contribution-container-title">
+              <p>Help us Improve!</p>
+            </div>
+            <div className="contribution-container-text">
+              <p>Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. Here goes text. </p>
+            </div>
+            <div className="contribution-container-button">
+              <button type="button"><span>Participate!</span></button>
+            </div>
+          </div>
+          
+        </div>
+        
+        {/* <div className="title">
+          <p>Help us Improve!</p>
+        </div> */}
       </div>
     )
     
