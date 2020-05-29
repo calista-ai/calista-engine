dbb39f754c6382fb46770dc0329045ae826fc17e
Alex Delitzas <alex.delitzas@gmail.com>
2020-05-29 17:37:28 +0300

    Change Loading Display

--- front-end/src/components/LoadingDisplay.js
+++ front-end/src/components/LoadingDisplay.js
@@ -1,47 +1,50 @@
 import React, { Component } from 'react'
 import { css } from '@emotion/core'
-import PropagateLoader from 'react-spinners/PropagateLoader'
-import Logo from '../assets/logo_transparent.png'
-
-
+// import PropagateLoader from 'react-spinners/PropagateLoader'
+import MoonLoader from 'react-spinners/MoonLoader'
 import '../styles/LoadingDisplay.css'
 
 
 class LoadingDisplay extends Component {
 
   render() {
-    const override = css`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    `
+    // const override = css`
+    //   display: flex;
+    //   justify-content: center;
+    //   align-items: center;
+    // `
 
     const webpageName = this.props.webpage
 
     return (
-      <div>
-        <div className="wrapper">
+      <div className="loading-section">
 
-          <div className="logo">
-            <img src={Logo} alt="logo"/>
-          </div>
-          <div className="spinner">
-            <PropagateLoader
-              css={override}
-              sizeUnit={"px"}
-              size={15}
-              color={'#8ecedf'}
-              loading={true}
-            />
-          </div>
-
-          <div className="loading-message">
-            <span>Evaluating the webpage</span>
-            <br />
-            <span className="webpage">{webpageName}</span>
-          </div>
-
+        <div className="logo">
+          <span>Calista</span>
         </div>
+        <div className="spinner">
+          {/* <PropagateLoader
+            css={override}
+            sizeUnit={"px"}
+            size={15}
+            color={'#FFFFFF'}
+            loading={true}
+          /> */}
+          <MoonLoader
+            // css={override}
+            size={70}
+            // margin={50}
+            color={'#FFFFFF'}
+            loading={true}
+          />
+        </div>
+
+        <div className="loading-message">
+          <span>Evaluating the webpage</span>
+          <br />
+          <span className="webpage">{webpageName}</span>
+        </div>
+
       </div>
     )
   }
