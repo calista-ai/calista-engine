902019602952f27422305b4dd8c65b2b02b87dd1
Alex Delitzas <alex.delitzas@gmail.com>
2020-05-29 19:22:46 +0300

    Uninstall package

--- front-end/src/components/IntroPageDisplay.js
+++ front-end/src/components/IntroPageDisplay.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react'
-import ReactPageScroller from "react-page-scroller";
 
 import SearchDisplay from "./SearchDisplay.js";
-
 import HowItWorksDisplay from "./HowItWorksDisplay.js"
-
 import InfoDisplay from "./InfoDisplay.js"
-
 import ContributionDisplay from "./ContributionDisplay.js"
-
 import Footer from "./Footer.js"
 
 class IntroPageDisplay extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { currentPage: null };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { currentPage: null };
+    // }
     
-    handlePageChange = number => {
-        this.setState({ currentPage: number }); // set currentPage number, to reset it from the previous selected.
-    };
+    // handlePageChange = number => {
+    //     this.setState({ currentPage: number }); // set currentPage number, to reset it from the previous selected.
+    // };
 
 
     render() {
         return (
             <div>
                 <div>
-
                     <SearchDisplay />
                     <HowItWorksDisplay />
                     <InfoDisplay />
