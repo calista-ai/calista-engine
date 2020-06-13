8e42f18921160bb503bf1a112c4d7a91d1ad4fa7
Alex Delitzas <alex.delitzas@gmail.com>
2020-06-13 20:13:06 +0300

    Clean code for Loading Display

--- front-end/src/components/LoadingDisplay.js
+++ front-end/src/components/LoadingDisplay.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import { css } from '@emotion/core'
-// import PropagateLoader from 'react-spinners/PropagateLoader'
 import MoonLoader from 'react-spinners/MoonLoader'
 import '../styles/LoadingDisplay.css'
 
@@ -8,32 +6,17 @@ import '../styles/LoadingDisplay.css'
 class LoadingDisplay extends Component {
 
   render() {
-    // const override = css`
-    //   display: flex;
-    //   justify-content: center;
-    //   align-items: center;
-    // `
 
-    const webpageName = this.props.webpage
+    const webpageName = this.props.webpage || 'https://www.example.com' // ToDO: remove
 
     return (
       <div className="loading-section">
-
         <div className="logo">
           <span>Calista</span>
         </div>
         <div className="spinner">
-          {/* <PropagateLoader
-            css={override}
-            sizeUnit={"px"}
-            size={15}
-            color={'#FFFFFF'}
-            loading={true}
-          /> */}
           <MoonLoader
-            // css={override}
             size={70}
-            // margin={50}
             color={'#FFFFFF'}
             loading={true}
           />
